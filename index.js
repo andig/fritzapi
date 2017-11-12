d4ada71e52ddcdb6f6fcbd3794f8a32832e182ce
andig <cpuidle@gmx.de>
2017-11-12 14:11:07 +0100

    Have getOSVersion reject if version not found

--- index.js
+++ index.js
@@ -381,7 +381,7 @@ module.exports.getOSVersion = function(sid, options)
         var json = JSON.parse(body);
         var osVersion = json.data && json.data.fritzos && json.data.fritzos.nspver
             ? json.data.fritzos.nspver
-            : null;
+            : Promise.reject();
         return osVersion;
     });
 };
