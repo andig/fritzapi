5a7a18ee0f2efb0ea247b883b5a2ce1f988dbc6d
foxthefox <github@wysiwis.net>
2018-11-13 21:52:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -99,13 +99,18 @@ Fritz.prototype = {
     getTemplateListInfos : function() {
         return this.call(module.exports.getTemplateListInfos );
     },
-
-    setTemplate: function(ain) {
-        return this.call(module.exports.setTemplate, ain);
+    applyTemplate: function(ain) {
+        return this.call(module.exports.applyTemplate, ain);
     },
     
-    getDeviceListInfos: function() {
-        return this.call(module.exports.getDeviceListInfos);
+    //new functions related to templates in version 7
+    getTemplateListInfos : function() {
+        return this.call(module.exports.getTemplateListInfos );
+    },
+    
+    //new functions related to devices in version 7
+    getBasicDeviceStats: function() {
+        return this.call(module.exports.getBasicDeviceStats);
     },
 
     getDeviceList: function() {
@@ -386,13 +391,32 @@ module.exports.getTemplateListInfos  = function(sid, options)
     return executeCommand(sid, 'gettemplatelistinfos', null, options);
 };
 
+// get template information (json)
+module.exports.getTemplateList = function(sid, options)
+{
+    return module.exports.getTemplateListInfos(sid, options).then(function(templateinfo) {
+        var templates = parser.xml2json(templateinfo);
+        // extract templates as array
+        templates = [].concat((templates.templatelist || {}).template || []).map(function(template) {
+            return template;
+        });
+        return templates;
+    });
+};
+
 // apply template
-module.exports.setTemplate = function(sid, ain, options)
+module.exports.applyTemplate = function(sid, ain, options)
 {
     return executeCommand(sid, 'applytemplate', ain, options).then(function(body) {
         var patt = new RegExp(ain);
         return patt.test(body); // returns applied ain if success -> true if successful
     });
+};
+
+// get basic device info (XML)
+module.exports.getBasicDeviceStats  = function(sid, options)
+{
+    return executeCommand(sid, 'getbasicdevicestats', null, options);
 };
 
 // get detailed device information (XML)
